6eb2cf0e5903a7a847f10fbd9dcd119af28587d2
JasonTKhuu <123114178+JasonTKhuu@users.noreply.github.com>
2023-01-19 14:58:15 -0600

    Add files via upload

---
+++ HowToCode/src/something/RiceisCorrect.java
@@ -0,0 +1,17 @@
+package something;
+
+public class RiceisCorrect {
+
+	public static void main(String[] args) {
+		
+		int firstNumber = 10;
+		
+		int secondNumber = 20;
+		
+		int addResult = firstNumber + secondNumber; 
+		
+		System.out.println("Addition Result: " + addResult); 
+
+	}
+
+}
