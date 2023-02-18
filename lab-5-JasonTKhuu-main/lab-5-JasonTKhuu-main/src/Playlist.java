051e99b69e2b4d06d88c05bbc3e2fd01851e5336
JasonTKhuu <123114178+JasonTKhuu@users.noreply.github.com>
2023-02-18 04:25:11 -0600

    Add files via upload

--- lab-5-JasonTKhuu-main/lab-5-JasonTKhuu-main/src/Playlist.java
+++ lab-5-JasonTKhuu-main/lab-5-JasonTKhuu-main/src/Playlist.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner; 
 
 public class Playlist {
 	
@@ -6,6 +9,11 @@ public class Playlist {
 	
 	public Playlist() {
 		this.songs = new ArrayList<Song>(); 
+	}
+	
+	public Playlist(String filename) throws FileNotFoundException {
+		this.songs = new ArrayList<Song>(); 
+		addSongs(filename); 
 	}
 	
 	public int getNumSongs() {
@@ -29,10 +37,48 @@ public class Playlist {
 	
 	public boolean addSong(int index, Song song) {
 		// TODO: Update the Lab 3 method.
+		if (song == null) {
+			return false;
+		}
+		else if (index < 0 || index >= getNumSongs() + 1) {
+			return false;
+		}
+		else {
+			this.songs.set(index, song); 
+			return true;
+		}
+	}
+	
+	public int addSongs(String filename) throws FileNotFoundException {
+		
+		Scanner input = new Scanner(new File(filename)); 
+		Playlist playlist = new Playlist(); 
+		int numSongsAdded = 0; 
+		
+		while (input.hasNextLine()) {
+			String song = input.nextLine(); 
+			playlist.addSongs(song); 
+			numSongsAdded = 0; 
+		}
+		
+		return numSongsAdded;
 	}
 	
 	public int addSongs(Playlist playlist) {
-		// TODO: Update the Lab 3 method.
+		
+		int numOfSongs = 0;
+		
+		if (playlist == null) {
+			return 0; 
+		}
+		
+		for (int index = 0; index < getNumSongs(); ++index) {
+			if (addSong(index, getSong(index)) == false) {
+				playlist.addSong(songs.get(index));
+				++numOfSongs; 
+			}
+		}
+		return numOfSongs; 
 	}
 	
 	public Song removeSong() {
@@ -41,5 +87,24 @@ public class Playlist {
 	
 	public Song removeSong(int index) {
 		// TODO: Update the Lab 3 method.
+		if (index < 0 || index >= getNumSongs() + 1) {
+			return null;
+		}
+		
+		return songs.remove(index); 
+	}
+	
+	public void saveSongs(String filename) throws FileNotFoundException {
+
+	}
+	
+	public int[] getTotalTime() {
+		
+		return null; // Stub
+	}
+	
+	public String toString() {
+		
+		return ""; // Stub 
 	}
 }
