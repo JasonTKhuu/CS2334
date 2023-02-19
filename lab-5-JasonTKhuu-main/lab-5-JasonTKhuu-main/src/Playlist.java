42df5253af736ec7d73ac448bfebc3363fcbbd93
JasonTKhuu <123114178+JasonTKhuu@users.noreply.github.com>
2023-02-19 13:34:08 -0600

    Add files via upload

--- lab-5-JasonTKhuu-main/lab-5-JasonTKhuu-main/src/Playlist.java
+++ lab-5-JasonTKhuu-main/lab-5-JasonTKhuu-main/src/Playlist.java
@@ -12,7 +12,7 @@ public class Playlist {
 	}
 	
 	public Playlist(String filename) throws FileNotFoundException {
-		this.songs = new ArrayList<Song>(); 
+		this();
 		addSongs(filename); 
 	}
 	
@@ -37,27 +37,59 @@ public class Playlist {
 	
 	public boolean addSong(int index, Song song) {
 		// TODO: Update the Lab 3 method.
-		if (song == null) {
-			return false;
-		}
-		else if (index < 0 || index >= getNumSongs() + 1) {
+		if (song == null || index < 0 || index > getNumSongs() + 1) {
 			return false;
 		}
 		else {
+			songs.add(null); 
 			this.songs.set(index, song); 
 			return true;
 		}
+		
 	}
 	
 	public int addSongs(String filename) throws FileNotFoundException {
 		
 		Scanner input = new Scanner(new File(filename)); 
-		Playlist playlist = new Playlist(); 
+		
 		int numSongsAdded = 0; 
 		
 		while (input.hasNextLine()) {
+			
+			int seconds = 0;
+			int minutes = 0;
+			int hour = 0;
+			
 			String song = input.nextLine(); 
-			playlist.addSongs(song); 
+		    String[] songInfo = song.split("; "); 
+		    
+		    String[] timeInfo = songInfo[2].split(":"); 
+		    int[] time = new int[timeInfo.length]; 
+		    
+		    if (time.length == 1) {
+		    	seconds = Integer.parseInt(timeInfo[0]);
+		    	time[0] = seconds;
+		    }
+		    else if (time.length == 2) {
+		    	minutes = Integer.parseInt(timeInfo[0]);
+		    	seconds = Integer.parseInt(timeInfo[1]); 
+		    	
+		    	time[0] = seconds;
+		    	time[1] = minutes; 
+		    }
+		    else {
+		    	minutes = Integer.parseInt(timeInfo[0]);
+		    	seconds = Integer.parseInt(timeInfo[1]); 
+		    	hour = Integer.parseInt(timeInfo[2]); 
+		    	
+		    	time[0] = seconds;
+		    	time[1] = minutes; 
+		    	time[2] = hour; 
+		    }
+		    
+		    Song newSong = new Song(songInfo[0], songInfo[1], time); 
+		    songs.add(newSong);
+		    
 			numSongsAdded = 0; 
 		}
 		
