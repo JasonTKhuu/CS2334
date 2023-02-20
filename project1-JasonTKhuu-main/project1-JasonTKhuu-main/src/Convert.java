7e0a8bfbb6f23a522e8a827b6e157b3ce8043ef5
JasonTKhuu <123114178+JasonTKhuu@users.noreply.github.com>
2023-02-20 15:17:08 -0600

    Add files via upload

--- project1-JasonTKhuu-main/project1-JasonTKhuu-main/src/Convert.java
+++ project1-JasonTKhuu-main/project1-JasonTKhuu-main/src/Convert.java
@@ -8,22 +8,41 @@ public class Convert {
 	
 	public static void convertFile(String filename) throws IOException {
 		Scanner input = new Scanner(new File(filename)); 
-		PrintWriter write = new PrintWriter(new FileWriter(filename)); 
+		
+		File outputFile = new File("triplog.csv"); 
+		
+		PrintWriter write = new PrintWriter(new FileWriter(outputFile)); 
 		
 		String lat = "";
 		String lon = "";
 		int increment = 0; 
 		
+		write.print("Time, " + "Latitude, " + "Longitude");
+		write.println(); 
 		while (input.hasNextLine()) {
 			String info = input.nextLine(); 
+			
 			if (info.contains("<trkpt")) {
-				lat = info.replaceAll("<trkpt lat=\"\" >", "").trim();
-				lon = info.replaceAll("<trkpt lon=\"\" >", "").trim();
 				
-				write.print(increment + ", " + lat + ", " + lon); 
+				lat = info.replaceAll("<trkpt", "");
+				lon = info.replaceAll("<trkpt", "");
+				lat = lat.replaceAll("lat=", "");
+				lon = lon.replaceAll("lon=", ""); 
+				lat = lat.replaceAll(" ", ""); 
+				lon = lon.replaceAll(" ", ""); 
+				lat = lat.replaceAll("\"", ""); 
+				lon = lon.replaceAll("\"", ""); 
+				lat = lat.replaceAll(">", "");
+				lon = lon.replaceAll(">", ""); 
+				
+				String output = increment + ", " + lat + ", " + lon;
+				
+				write.print(output); 
+				write.println();
 				increment += 5; 
 			}
 		}
 		write.close(); 
+		input.close(); 
 	}
 }
